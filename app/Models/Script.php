a1349b7f88adf3c4d405b2d610a49f4246badbd1
Dimas Ahmad Rizal <mail.dimasrizal@gmail.com>
2025-12-10 23:54:42 +0700

    fix: add is_success to script table and fix model

--- app/Models/Script.php
+++ app/Models/Script.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Script extends Model
 {
     protected $table = 'scripts';
-    protected $fillable = ['name', 'script'];
+    protected $fillable = [
+        'name',
+        'script',
+        'is_success',
+    ];
+
+    protected $casts = [
+        'is_success' => 'boolean',
+    ];
 }
