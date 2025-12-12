8a1630f07bd454d1a8070fc60b52f9725c233091
Dimas Ahmad Rizal <mail.dimasrizal@gmail.com>
2025-12-12 11:41:27 +0700

    fix: add route name for the index page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Index', ['title' => 'Script Tracking System']);
-});
+})->name('index');
 Route::get('/scripts', [ScriptController::class, 'index'])->name('scripts.index');
 Route::post('/scripts/create', [ScriptController::class, 'store'])->name('scripts.store');
 Route::put('/scripts/update/{id}', [ScriptController::class, 'update'])->name('scripts.update');
